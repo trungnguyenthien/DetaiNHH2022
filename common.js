02d5495cbfa32fcac92c3551d9b6c479febc6941
Trung Nguyen Thien <ngthientrung@gmail.com>
2022-12-04 22:32:44 +0700

    no message

--- common.js
+++ common.js
@@ -131,3 +131,29 @@ function getCookie(name, defval = 0) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+function parse_query_string(query) {
+    // var query = window.location.search.substring(1);
+    // var parsed_qs = parse_query_string(query);
+    // console.log(parsed_qs.c);
+    var vars = query.split("&");
+    var query_string = {};
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      var key = decodeURIComponent(pair.shift());
+      var value = decodeURIComponent(pair.join("="));
+      // If first entry with this name
+      if (typeof query_string[key] === "undefined") {
+        query_string[key] = value;
+        // If second entry with this name
+      } else if (typeof query_string[key] === "string") {
+        var arr = [query_string[key], value];
+        query_string[key] = arr;
+        // If third or later entry with this name
+      } else {
+        query_string[key].push(value);
+      }
+    }
+    return query_string;
+  }
+  
